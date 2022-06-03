ccb504a2b8927d3bc25e30960619c2ac2f318e09
Manikanta7013 <98421090+Manikanta7013@users.noreply.github.com>
2022-06-03 11:00:42 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,28 +1,30 @@
-function age(){
-    let firstdate=document.getElementById("date").value;
-    let firstmonth=document.getElementById("month").value;
-    let firstyear=document.getElementById('year').value;
-    let data = new Date();
-    let endDate=data.getDate();
-    let endMonth=1+data.getMonth();
-    let endYear=data.getFullYear();
-    let arr=[31,28,31,30,31,30,31,31,30,31,30,31]
-    if(firstmonth > endMonth ){
-      
-      endMonth = endMonth+11;
-      endYear = endYear-1;
-      
-    }
-    if(firstdate>endDate){  //  05 01
-      var date=arr[endDate-1]-firstdate
-    }else{
-    
-    var date=endDate-firstdate;
-    }
-    let month=endMonth-firstmonth;
-    let year=endYear-firstyear;
-    
-    
-    document.getElementById('result').innerHTML = `${year}years ${month} Months ${date}days `;
-    
+function age() {
+  var firstdate = document.getElementById('date').value;
+  var firstmonth = document.getElementById('month').value;
+  var firstyear = document.getElementById('year').value;
+
+  var date = new Date();
+  var enddate = date.getDate();
+  //console.log(date);
+  var endmonth= 1+date.getMonth();
+  //console.log(m2);
+  var endyear = date.getFullYear();
+  var arr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+                                  // 5 11 1999   to 3 6 2022
+  if(firstdate > enddate){       //  5>3
+    enddate = enddate + arr[endmonth - 1];
+                        // d2=3+30=33
+    endmonth = endmonth - 1;
+    //console.log(m2);      // m2=5
   }
+  if(firstmonth > endmonth){                 // 11>5
+    endmonth = endmonth + 12;              // 17
+    endyear = endyear - 1;                // 2021
+  }
+  var d = enddate - firstdate;  
+    //console.log(d1);                               // 33-3
+  var m = endmonth - firstmonth;
+  var y = endyear - firstyear;
+
+  document.getElementById('age').innerHTML = 'Your Age is '+y+' Years '+m+' Months '+d+' Days';
+}
