b5f396fb3032e838ed0294e37c3cdbf244bb2a73
Manikanta7013 <98421090+Manikanta7013@users.noreply.github.com>
2022-06-18 14:08:35 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,36 +1,36 @@
 function age(){
-    let firstdate=document.getElementById("date").value; // 05
+    let firstdate=document.getElementById("date").value; 
     
-    let firstmonth=document.getElementById("month").value;  // 11
+    let firstmonth=document.getElementById("month").value;  
     
     
-    let firstyear=document.getElementById('year').value;    // 1999
+    let firstyear=document.getElementById('year').value;    
     var arr=[31,28,31,30,31,30,31,31,30,31,30,31];
-    //document.write(arr[1]);
+  
     
     if(firstdate>=32 || firstmonth>=13 ||firstdate>arr[1]){
       alert("error");
     }else{            
       var date = new Date();
       var enddate = date.getDate();
-      //console.log(date);
+     
       var endmonth= 1+date.getMonth();
-      //console.log(m2);
+     
       var endyear = date.getFullYear();
      
-                                      // 5 11 1999   to 3 6 2022
-      if(firstdate > enddate){       //  5>3
+                                      
+      if(firstdate > enddate){       
         enddate = enddate + arr[endmonth - 1];
-                            // d2=3+30=33
+                            
         endmonth = endmonth - 1;
-        //console.log(m2);      // m2=5
+            
       }
-      if(firstmonth > endmonth){                 // 11>5
-        endmonth = endmonth + 12;              // 17
-        endyear = endyear - 1;                // 2021
+      if(firstmonth > endmonth){                
+        endmonth = endmonth + 12;             
+        endyear = endyear - 1;               
       }
       var d = enddate - firstdate;  
-        //console.log(d1);                               // 33-3
+                                     
       var m = endmonth - firstmonth;
       var y = endyear - firstyear;
     
